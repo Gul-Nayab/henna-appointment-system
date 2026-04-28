e25eb408f3666c5b05c13540ab4837cb23d23651
Gul Nayab <gngulnayab@gmail.com>
2026-04-27 18:17:04 -0700

    create project with springboot java backend.

---
+++ backend/src/main/java/edu/sjsu/cmpe172/henna/controller/HomeController.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cmpe172.henna.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "This is the home page";
+    }
+}
