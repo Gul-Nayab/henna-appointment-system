232c4888f21d8b0e54482d4938ca7c43736f26b6
Gul Nayab <gngulnayab@gmail.com>
2026-04-29 00:24:12 -0700

    create a service card component to display information for a service.

---
+++ frontend/components/serviceCard.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import Image from 'next/image';
+
+interface Service {
+  serviceId: number;
+  type: string;
+  duration: number;
+  price: number;
+  description: string;
+}
+
+function formatDuration(duration: number) {
+  if (duration < 60) {
+    return `${duration} min`;
+  }
+
+  if (duration === 60) {
+    return '1 hr';
+  }
+
+  if (duration % 60 === 0) {
+    return `${duration / 60} hr`;
+  }
+
+  const hours = Math.floor(duration / 60);
+  const minutes = duration % 60;
+  return `${hours} hr ${minutes} min`;
+}
+
+function ServiceCard({ service }: { service: Service }) {
+  const imageSrc = `/images/services/service_${service.type.toLowerCase()}_${service.serviceId}.png`;
+
+  return (
+    <article className='service-card'>
+      <div className='service-image-wrapper'>
+        <Image
+          src={imageSrc}
+          alt={service.type}
+          className='service-image'
+          width={100}
+          height={100}
+        />
+      </div>
+
+      <div className='service-card-content'>
+        <h3 className='service-name'>{service.type}</h3>
+        <p className='service-description'>
+          {service.description} - {formatDuration(service.duration)}
+        </p>
+        <p className='service-price'>${service.price}</p>
+      </div>
+    </article>
+  );
+}
+
+export default ServiceCard;
