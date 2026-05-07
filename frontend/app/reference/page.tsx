18db0bf1b388f319ffc6038fbd43db9996981fcb
Gul-Nayab <gngulnayab@gmail.com>
2026-05-07 15:47:06 -0700

    create dashboard and reference pages.

---
+++ frontend/app/reference/page.tsx
@@ -0,0 +1,14 @@
+import NavBar from '@/components/Navbar';
+import Services from '@/components/services';
+
+function Reference() {
+  return (
+    <>
+      <NavBar />
+      <h1>Reference</h1>
+      <Services />
+    </>
+  );
+}
+
+export default Reference;
